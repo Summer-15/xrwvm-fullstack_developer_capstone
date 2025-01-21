ea04401e33d53ff47847aead7f7ef40025c22a6a
summer-15 <summer-15>
2025-01-21 09:02:15 -0500

    fixed lint fixes 4

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://artemkishuba-8000.'+
-    'theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://artemkishuba-8000'+
-    '.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
